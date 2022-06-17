bf739e1e151cba84e681773b6314459dba08adfe
Konrad_Cieslak_nr_albumu_11349 <konrad6dd@gmail.com>
2022-06-17 15:00:52 +0200

    Initial commit

---
+++ src/main/java/pl/crud_app/movielibrary/MovieLibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.crud_app.movielibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLibraryApplication.class, args);
+	}
+
+}
