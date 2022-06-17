7a0ddecf6e5ad78ff757b59bfbee9a4e03905195
Konrad_Cieslak_nr_albumu_11349 <konrad6dd@gmail.com>
2022-06-17 15:59:40 +0200

    Added Patch and Delete methods

--- src/main/java/pl/crud_app/movielibrary/MovieController.java
+++ src/main/java/pl/crud_app/movielibrary/MovieController.java
@@ -40,4 +40,24 @@ public class MovieController {
             return -1;
         }
     }
+
+    @PatchMapping("/{id}")
+    public int partiallyUpdate(@PathVariable("id") int id, @RequestBody Movie updatedMovie) {
+        Movie movie = movieRepository.getById(id);
+
+        if (movie != null){
+            if (updatedMovie.getName() != null) movie.setName(updatedMovie.getName());
+            if (updatedMovie.getRating() > 0) movie.setRating(updatedMovie.getRating());
+
+            movieRepository.update(movie);
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public int delete(@PathVariable("id") int id) {
+        return movieRepository.delete(id);
+    }
 }
